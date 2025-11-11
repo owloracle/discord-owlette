3a32aa5789748eda2f24543ba9e0acf1df623585
alexnaz <alexander.nazarenko0703@gmail.com>
2025-11-11 03:22:33 +0100

    Deleted eslint TODO: fix eslint issues that interrupt program
    Added cooldowns to bot commands

--- commands/utility/hoot.js
+++ commands/utility/hoot.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
+	cooldown: 5,
 	data : new SlashCommandBuilder().setName('hoot').setDescription('Replies with a "Hoot Hoot!" message'),
 	async execute(interaction) {
 		await interaction.reply('Hoot Hoot!');
